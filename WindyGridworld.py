1e3e7866b7ca8115a6ea1fb87617bc5b570a0dfd
LuisChore <luis.vazquez.ch@hotmail.com>
2021-04-15 19:26:39 -0500

    Added penalized gridworld

--- WindyGridworld.py
+++ WindyGridworld.py
@@ -88,3 +88,61 @@ def get_windygridworld():
     g = WindyGridworld(n,m,start_position)
     g.set(rewards,walls,terminal_states,actions,transition_probabilities)
     return g
+
+
+def get_penalized_windygridworld(step_cost = 0):
+    n = 3
+    m = 4
+    start_position = (2,0)
+    terminal_states = ((0,3),(1,3))
+    walls = ((1,1))
+
+    rewards = {}
+    for i in range(n):
+        for j in range(m):
+            rewards[(i,j)] = step_cost
+
+    rewards[(0,3)] = 1
+    rewards[(1,3)] = -1
+
+    actions = {
+        (0,0):(1,2),
+        (0,1):(1,3),
+        (0,2):(1,2,3),
+        (1,0):(0,2),
+        (1,2):(0,1,2),
+        (2,0):(0,1),
+        (2,1):(1,3),
+        (2,2):(0,1,3),
+        (2,3):(0,3),
+    }
+
+    # p(s'|s,a)
+    # Key: (s,a) -> Value: {s': p(s',s,a)}
+    transition_probabilities = {
+        ((0,0),1): {(0,1):1.0},
+        ((0,0),2): {(1,0):1.0},
+        ((0,1),1): {(0,2):1.0},
+        ((0,1),3): {(0,0):1.0},
+        ((0,2),1): {(0,3):1.0},
+        ((0,2),2): {(1,2):1.0},
+        ((0,2),3): {(0,1):1.0},
+        ((1,0),0): {(0,0):1.0},
+        ((1,0),2): {(2,0):1.0},
+        ((1,2),0): {(0,2):0.5,(1,3):0.5},
+        ((1,2),1): {(1,3):1.0},
+        ((1,2),2): {(2,2):1.0},
+        ((2,0),0): {(1,0):1.0},
+        ((2,0),1): {(2,1):1.0},
+        ((2,1),1): {(2,2):1.0},
+        ((2,1),3): {(2,0):1.0},
+        ((2,2),0): {(1,2):1.0},
+        ((2,2),1): {(2,3):1.0},
+        ((2,2),3): {(2,1):1.0},
+        ((2,3),0): {(1,3):1.0},
+        ((2,3),3): {(2,2):1.0},
+    }
+
+    g = WindyGridworld(n,m,start_position)
+    g.set(rewards,walls,terminal_states,actions,transition_probabilities)
+    return g
